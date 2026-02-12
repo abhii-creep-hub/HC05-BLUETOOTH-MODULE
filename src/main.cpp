e0902f4fd9785e155b87567a31a29c61630006b3
abhii-creep-hub <abhinav.sharma_cs.aiml24@gla.ac.in>
2026-02-12 09:22:16 +0530

    Add Bluetooth communication setup and configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
 #include <Arduino.h>
+
 void setup() {
+    Serial.begin(38400); //PC SERIAL MONITOR
+    Serial1.begin(38400); //HC-05 AT MODE
+
 // write your initialization code here
 }
 
 void loop() {
+    //PC TO BLUETOOTH
+    if (Serial.available()) {
+        Serial1.write(Serial.read());
+    }
+    //BLUETOOTH TO PC
+    if (Serial1.available()) {
+        Serial.write(Serial1.read());
+    }
 // write your code here
 }
